5de41e308af9ece3348b64a916ccc5a73024bf08
Jason Chaney <bphett@users.noreply.github.com>
2024-10-02 16:29:14 -0500

    Update routes.go
    Trying -async 1 instead of -copyts

--- routes.go
+++ routes.go
@@ -199,7 +199,7 @@ func stream(lineup *lineup) gin.HandlerFunc {
 			defer resp.Body.Close()
 			
 			log.Infoln("Remuxing stream with ffmpeg")
-			run := exec.Command("ffmpeg", "-i", "pipe:0", "-copyts", "-start_at_zero", "-c:v", "copy", "-c:a", "copy", "-f", "mpegts", "pipe:1")
+			run := exec.Command("ffmpeg", "-i", "pipe:0", "-async", "1", "-c:v", "copy", "-c:a", "copy", "-f", "mpegts", "pipe:1")
 			log.Debugf("Executing ffmpeg as \"%s\"", strings.Join(run.Args, " "))
 
 			ffmpegin, err := run.StdinPipe()
